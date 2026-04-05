52b398f6c9b71ffe08f0dc92f30e8e5f1c921127
Raye Rayne <247046349+21leahcimhtiek-oss@users.noreply.github.com>
2026-04-04 17:36:09 -0700

    feat: add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,35 @@
+/** @type {import('next').NextConfig} */
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000"],
+    },
+  },
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: process.env.NEXT_PUBLIC_APP_URL || "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "Authorization,Content-Type" },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = withSentryConfig(nextConfig, {
+  silent: true,
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+});
